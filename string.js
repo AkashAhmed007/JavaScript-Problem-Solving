40b57e9361357cce135afe6ec338c791bdcd686e
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-02-03 10:31:21 +0600

    charAtIndex found

--- string.js
+++ string.js
@@ -6,4 +6,20 @@ function stringLength(str){
 
 const sentence = "I am a Web Developer";
 const length = stringLength(sentence);
-console.log(length)
+//console.log(length)
+
+
+// Create a program that prompts the user for a string and an index. Then, use the charAt() method to display the character at that index.
+
+function CharAtIndex(str,index){
+    let indexNumber = str.charAt(index);
+
+    return indexNumber;
+
+}
+
+let text = "I Love Programming";
+let index = 7;
+let result = CharAtIndex(text,index);
+console.log(result);
+
