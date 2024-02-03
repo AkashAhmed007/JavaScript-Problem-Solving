c4ebaab4995b6c1d7d9102d6a87426d677b7cc2e
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-02-03 10:10:36 +0600

    string length found!

--- string.js
+++ string.js
@@ -1,1 +1,9 @@
 // Write a program that takes a user's input and calculates the length of the input string using the length property.
+
+function stringLength(str){
+    return str.length;
+}
+
+const sentence = "I am a Web Developer";
+const length = stringLength(sentence);
+console.log(length)
